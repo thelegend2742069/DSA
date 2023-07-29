b24c86b5e505cb4b375086890dd7bcc41bec55fc
thelegend2742069 <blab8650@gmail.com>
2023-07-30 02:15:00 +0530

    added comments

--- Tower of Hanoi.py
+++ Tower of Hanoi.py
@@ -2,11 +2,12 @@
 
 def Hanoi(n, source, intermediate, destination):
     if not n:
+        #return if there are no more donuts to move
         return 0
     
-    Hanoi(n-1, source, destination, intermediate)
-    print(f'Moved donut {n} from {source} to {destination}')
-    Hanoi(n-1, intermediate, source, destination)
+    Hanoi(n-1, source, destination, intermediate)               #move donuts above current donut to intermediate tower
+    print(f'Moved donut {n} from {source} to {destination}')    #move current donut to destination
+    Hanoi(n-1, intermediate, source, destination)               #move remaining donuts from intermediate to destination
 
     return 0
 
