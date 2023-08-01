b059e9a47e670b32f6e2b6a8eba636a30ec39458
thelegend2742069 <blab8650@gmail.com>
2023-08-01 14:22:44 +0530

    checked exceptions for top and pop

--- Stacks/stack_test.py
+++ Stacks/stack_test.py
@@ -2,15 +2,19 @@ from stack import Stack
 
 number = Stack()
 
+print(number.top())
 number.push(5)
 number.push(3)
 number.push(6)
 number.push(1)
+print(number.top())
 
-print("len = ", len(number))
+print("len =", len(number))
 
 print(number.pop())
-print("len = ", len(number))
+print("len =", len(number))
 print(number.pop())
 print(number.pop())
+print(number.pop())
+
 print(number.pop())
