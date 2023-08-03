d6551a7e6b8b24e4f3737a39e360f1a846b2e231
thelegend2742069 <blab8650@gmail.com>
2023-08-03 18:43:58 +0530

    singly/doubly linked list base

---
+++ Linked List/doubly_linked_list.py
@@ -0,0 +1,40 @@
+#doubly linked list implementation
+
+class DoublyLinkedList:
+    #---------------------------------Node Class--------------------------------------
+    class _Node:
+        #stores element and next link for a node
+        
+        #__slots__ reduces memory usage
+        __slots__ = '_element', '_prev', '_next'
+
+        def __init__(self, element, prev, next):
+            self._element = element
+            self._prev = prev
+            self._next = next
+    
+    #----------------------------------methods----------------------------------------
+    def __init__(self) -> None:
+        self._header = self._Node(None, None, None)
+        self._trailer = self._Node(None, None, None)
+        self._header._next = self._trailer      #set header's next element as trailer
+        self._trailer._prev = self._header      #set trailer's next element as header
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+    
+    def is_empty(self):
+        return self._size == 0
+    
+    def add_node(self, element, predecessor, successor):
+        #inserts element between predecessor and successor
+        node = self._Node(element, predecessor, successor)
+        successor._prev = node
+        predecessor._next = node
+    
+    def delete_node(self, node, predecessor, successor):
+        #deletes node between predecessor and successor
+        node._prev = node._next = node._element = None
+        successor._prev = predecessor
+        predecessor._next = successor
---
+++ Linked List/singly_linked_list.py
@@ -0,0 +1,32 @@
+class SinglyLinkedList:
+    #---------------------------------Node Class--------------------------------------
+    class _Node:
+        #stores element and next link for a node
+        
+        #__slots__ reduces memory usage
+        __slots__ = '_element', '_next'
+
+        def __init__(self, element, next):
+            self._element = element
+            self._next = next
+        
+    #------------------------------internal methods------------------------------------
+    def __init__(self):
+        self._head = self._Node(None, None)
+        self._size = 0
+    
+    def __len__(self):
+        return self._size
+    
+    def is_empty(self):
+        return self._size == 0
+    
+    def add_node(self, element):
+        new = self._Node(element, self._head)
+        self._head = new
+    
+    def delete_node(self, node):
+        answer = node._element
+        self._head = node._next
+        node._element = node._next = None
+        return answer
