fbcd1dbf9a765c150df4972684438cc75c7f30ba
thelegend2742069 <blab8650@gmail.com>
2023-08-05 12:37:07 +0530

    comments

--- Trees/linked_binary_tree.py
+++ Trees/linked_binary_tree.py
@@ -77,7 +77,8 @@ class LinkedBinary(BinaryTree):
             num += 1
         return num
     
-    
+    #-------------------------------mutators---------------------------------
+    #add root node to tree
     def add_root(self, element):
         if self._root is not None:
             raise ValueError("root already exists")
@@ -86,10 +87,11 @@ class LinkedBinary(BinaryTree):
         self._size += 1
         return self.make_position(node)
     
+    #add left child to node and return its position
     def add_left(self, element, position):
+        parent = self._validate(position)                                       #validate position
 
-        parent = self._validate(position)
-        if parent._left is not None:
+        if parent._left is not None:                                            #check if child alread exists
             raise ValueError("left child already exists")
         
         left = self.Node(element, parent)
@@ -97,10 +99,11 @@ class LinkedBinary(BinaryTree):
         self._size += 1
         return self.make_position(left)
     
+    #add right child to node and return its position
     def add_right(self, element, position):
+        parent = self._validate(position)                                       #validate position
 
-        parent = self._validate(position)
-        if parent._right is not None:
+        if parent._right is not None:                                           #check if child alread exists
             raise ValueError("right child already exists")
         
         right = self.Node(element, parent)
@@ -108,20 +111,23 @@ class LinkedBinary(BinaryTree):
         self._size += 1
         return self.make_position(right)
     
+    #replace element at a node and return old element
     def replace(self, position, element):
         node = self._validate(position)
         old = node._element
         node._element = element
         return old
     
+    #delete node at position and return its element
     def delete(self, position):
-        node = self._validate(position)
-        if self.num_children(position):
+        node = self._validate(position)                                         #validate position
+
+        if self.num_children(position):                                         #check for multiple children
             raise ValueError("cannot delete node: has two children")
         
-        child = node._left if node._left else node._right
+        child = node._left if node._left else node._right                       #assign child
 
-        if self.is_root(node):
+        if self.is_root(node):                                                  #check is node is root
             self._root = child
         else:
             parent = node._parent
@@ -135,22 +141,23 @@ class LinkedBinary(BinaryTree):
 
         return node._element
     
+    #attach 2 trees to leaf node
     def attach(self, position, tree1, tree2):
-        leaf = self._validate(position)
+        leaf = self._validate(position)                                         #validate position
         
-        if not self.is_leaf(position):
+        if not self.is_leaf(position):                                          #ensure node is leaf
             raise ValueError("node already has children")
 
-        if type(self) is not type(tree1) or type(self) is not type(tree2):
+        if type(self) is not type(tree1) or type(self) is not type(tree2):      #ensure trees of same type
             raise TypeError("trees are not of same type")
         
-        if not tree1.is_empty():
+        if not tree1.is_empty():                                                #depreciate tree1 and attach to leaf
             tree1._root._parent = leaf
             leaf._left = tree1._root
             tree1._root = None
             tree1._size = 0
         
-        if not tree2.is_empty():
+        if not tree2.is_empty():                                                #depreciate tree2 and attach to leaf
             tree2._root._parent = leaf
             leaf._left = tree2._root
             tree2._root = None
