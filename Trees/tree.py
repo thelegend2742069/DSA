d9b0ceb1dc80cc745344a429864533e1cac9c1dc
thelegend2742069 <blab8650@gmail.com>
2023-08-09 22:52:26 +0530

    comments

--- Trees/tree.py
+++ Trees/tree.py
@@ -1,6 +1,6 @@
 #tree abstract class
-import sys
 from ..Queues.queue import Queues
+
 class Tree:
     #------------------------------Position Class------------------------------
     class Position:
@@ -49,7 +49,7 @@ class Tree:
         return self.num_children(p) == 0
     
     def positions(self):
-        #return a list of positions in the tree elements
+        #generate an iteration of all positions in the tree
         raise NotImplementedError("no traversal method selected to call")
     
     def __iter__(self):
@@ -77,38 +77,48 @@ class Tree:
     #----------------------------traversal methods-------------------------------
     
     def preorder_traverse(self):
+        #iterate over positions in preorder traversal
         if not self.is_empty():
             for p in self._subtree_preorder(self.root()):
                 yield p
 
     def _subtree_preorder(self, p):
+        #iterate over positions of a subtree rooted at p
+        #in preorder traversal
+
         yield p
         for c in self.children(p):
             for other in self._subtree_preorder(c):
                 yield other
 
     def postorder_traverse(self):
+        #iterate over positions in postorder traversal
         if not self.is_empty():
             for p in self._subtree_postorder(self.root()):
                 yield p
     
     def _subtree_postorder(self, p):
+        #iterate over positions of a subtree rooted at p
+        #in postorder traversal
+        
         for c in self.children(p):
             for other in self._subtree_postorder(c):
                 yield other
         yield p
     
     def breadth_first_traverse(self):
+        #iterate over positions in preorder traversal
+
+        #make a queue containing root node
         if not self.is_empty():
             node_queue = Queues()
             node_queue.enqueue(self.root())
         
         while not node_queue.is_empty():
+            #yield last node in queue
             p = node_queue.dequeue()
             yield p
 
+            #add children nodes in queue
             for c in self.children(p):
                 node_queue.enqueue(c)
-            
-
-    
